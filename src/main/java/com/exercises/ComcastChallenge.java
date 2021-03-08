38b29de25b634e08b064b20dc99eda7c1689ecf0
Eric Balawejder <eric.balawejder@gmail.com>
2021-03-07 20:23:06 -0500

    Refactored findFirstNonRepeatingCharacter() method and tests

--- src/main/java/com/exercises/ComcastChallenge.java
+++ src/main/java/com/exercises/ComcastChallenge.java
@@ -1,12 +1,7 @@
 package com.exercises;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -26,32 +21,16 @@ import java.util.stream.Collectors;
 public class ComcastChallenge {
 
     public Character findFirstNonRepeatingCharacter(String word) {
-
-        Set<Character> repeatingCharacters = new HashSet<>();
-        List<Character> nonRepeatingCharacters = new ArrayList<>();
-
-        for (int i = 0; i < word.length(); i++) {
-            Character character = word.charAt(i);
-            if (nonRepeatingCharacters.contains(character)) {
-                nonRepeatingCharacters.remove(character);
-                repeatingCharacters.add(character);
-            } else {
-                nonRepeatingCharacters.add(character);
-            }
-        }
-        return nonRepeatingCharacters.size() == 0 ? null : nonRepeatingCharacters.get(0);
-    }
-
-    // A better solution is to return an empty collection or an empty Optional rather than null.
-    public Optional<Character> findFirstNonRepeatingCharacterOptional(String word) {
         return word.chars()
                 .mapToObj(i -> (char) i)
+                .map(Character::toLowerCase)
                 .collect(Collectors.groupingBy(
                         Function.identity(), LinkedHashMap::new, Collectors.counting()))
                 .entrySet().stream()
                 .filter(entry -> entry.getValue() == 1L)
                 .map(Map.Entry::getKey)
-                .findFirst();
+                .findFirst()
+                .orElse(null);
     }
 
 }
--- src/test/java/com/exercises/ComcastChallengeTest.java
+++ src/test/java/com/exercises/ComcastChallengeTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ComcastChallengeTest {
 
@@ -29,51 +29,17 @@ public class ComcastChallengeTest {
 
     @Test
     public void testFindFirstNonRepeatedCharacterToot() {
-        assertEquals(null, challenge.findFirstNonRepeatingCharacter("toot"));
+        assertNull(challenge.findFirstNonRepeatingCharacter("toot"));
     }
 
-    // Case insensitivity is NOT implemented
-
-    // This will pass.
     @Test
     public void testFindFirstNonRepeatedCharacterCaseSensitive() {
-        assertEquals(Character.valueOf('S'), challenge.findFirstNonRepeatingCharacter("Sensitive"));
+        assertNotEquals(Character.valueOf('S'), challenge.findFirstNonRepeatingCharacter("Sensitive"));
     }
 
-    // This will fail.
-    @Disabled
     @Test
     public void testFindFirstNonRepeatedCharacterCaseInsensitive() {
         assertEquals(Character.valueOf('n'), challenge.findFirstNonRepeatingCharacter("Sensitive"));
-    }
-
-    //--------------------------------------------------------------------------------------------------
-    // Optional<Character> implementation tests.
-
-    @Test
-    public void testFindFirstNonRepeatedCharacterOptionalAirplane() {
-        assertEquals(Optional.of('i'), challenge.findFirstNonRepeatingCharacterOptional("airplane"));
-    }
-
-    @Test
-    public void testFindFirstNonRepeatedCharacterOptionalLlama() {
-        assertEquals(Optional.of('m'), challenge.findFirstNonRepeatingCharacterOptional("llama"));
-    }
-
-    @Test
-    public void testFindFirstNonRepeatedCharacterOptionalToot() {
-        assertEquals(Optional.empty(), challenge.findFirstNonRepeatingCharacterOptional("toot"));
-    }
-
-    @Test
-    public void testFindFirstNonRepeatedCharacterOptionalCaseSensitive() {
-        assertEquals(Optional.of('S'), challenge.findFirstNonRepeatingCharacterOptional("Sensitive"));
-    }
-
-    @Disabled
-    @Test
-    public void testFindFirstNonRepeatedCharacterOptionalCaseInsensitive() {
-        assertEquals(Optional.of('n'), challenge.findFirstNonRepeatingCharacterOptional("Sensitive"));
     }
 
 }
